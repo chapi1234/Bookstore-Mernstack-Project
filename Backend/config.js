932ec57a54e68555cc4ff00e7cefa1c6cb53d9fe
metasebiyaw asfaw <metasebiyawasfaw@gmail.com>
2024-08-11 21:05:52 -0900

    Update

--- Backend/config.js
+++ Backend/config.js
@@ -1,1 +1,3 @@
 export const PORT = 5555;
+
+export const mongoDBURL = 'mongodb://localhost:27017'
--- Backend/index.js
+++ Backend/index.js
@@ -1,15 +1,23 @@
-import express, { request, response } from "express"
-import {PORT} from "./config.js"
+import express from 'express';
+import { PORT, mongoDBURL } from './config.js';
+import mongoose from 'mongoose';
 
+const app = express();
 
-const app = express()
-
-app.get('/', (request,response) => {
-    console.log(request)
-    return response.status(234).send('Welcome To MERN Stack Tutorials')
+app.get('/', (request, response) => {
+    console.log(request);
+    return response.status(234).send('Welcome To MERN Stack Tutorials');
 });
 
-app.listen(PORT, () =>{
+app.listen(PORT, () => {
     console.log(`App is listening to port: ${PORT}`);
-})
+});
 
+mongoose
+    .connect(mongoDBURL)
+    .then(() => { 
+        console.log("App is connected to database server");
+    })
+    .catch((error) => {
+        console.log('failed to connect');
+});
